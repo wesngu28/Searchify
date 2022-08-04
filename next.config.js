cb117be7081ca99d88072ad381f114d90bb38bd0
wesngu028 <serpents123@gmail.com>
2022-08-04 00:31:38 -0700

    strict mode and more rewrites

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
-module.exports = () => {
-  const rewrites = () => {
+module.exports = {
+  reactStrictMode: true, 
+  rewrites: async() => {
     return [
       {
         source: "/search/:path*",
         destination: "http://127.0.0.1:5000/search/:path*",
       },
+      {
+        source: "/auth",
+        destination: "http://127.0.0.1:5000/auth",
+      },
+      {
+        source: "/user",
+        destination: "http://127.0.0.1:5000/user",
+      },
     ];
-  };
-  const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-  };
-  return {
-    rewrites
-  };
+  }
 };
