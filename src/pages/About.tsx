4ade684213c878f3a1f196014bb013e90f093ac7
wesngu028 <serpents123@gmail.com>
2022-08-06 23:28:52 -0700

    about page and padding

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,20 @@
+import aboutStyles from '../styles/About.module.css'
+
 export default function About() {
-    return <h1>Any in the chat !</h1>
+    return (
+        <>
+            <h1>About this Web App</h1>
+            <div className={aboutStyles.information}>
+                <p>Searchify was the first real project I made since getting back into programming.</p>
+                <p>Its first iteration was a Python Flask app that served templates as well as acted as a backend communicator to the Spotify API.
+                The user would interact with the app through a Tkinter interface, and I expanded to include Pysimplegui as well.
+                This worked for me at the time but I wanted the app to be more usable to non-programmers, but at the time I was not familiar with web dev or its technologies.
+                After taking a web development course at my university, I saw a path to transitioning Searchify into a separate frontend website with a Python Flask backend.</p>
+                <p>I considered caching the playlist responses and user data in a Redis database (rather than traditional databases as the data always changes),
+                but decided against it.</p>
+                <p>Technologies used: HTML, CSS, Typescript, Node.js, React, Next.js, Chart.js, Python, Flask, Vite/Tkinter/Pysimplegui (previous renditions)
+                </p>
+            </div>
+        </>
+    )
 }
