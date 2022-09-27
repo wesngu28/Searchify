ad8cc66824a0762dd4276d416d622f6eb674fccb
wesngu28 <serpents123@gmail.com>
2022-09-26 23:16:31 -0700

    simplify

--- src/components/Metadata.tsx
+++ src/components/Metadata.tsx
@@ -1,26 +1,14 @@
 import Head from 'next/head'
 
-interface Props {
-    title?: string;
-    keywords?: string;
-    description?: string;
-}
-
-export default function Metadata ({ title, keywords, description }: Props) {
+export default function Metadata () {
     return(
         <Head>
-            <title>{title}</title>
-            <meta name='keywords'>{keywords}</meta>
-            <meta name='description'>{description}</meta>
+            <title>Searchify</title>
+            <meta name='keywords'>spotify stats, music</meta>
+            <meta name='description'>Get your top Spotify songs and artists and other Spotify information</meta>
             <meta charSet="UTF-8" />
             <link rel="icon" type="image/svg+xml" href="/public/favicon.ico" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </Head>
     )
-}
-
-Metadata.defaultProps = {
-    title: 'Searchify',
-    keywords: 'spotify stats, music',
-    description: 'Get your top Spotify songs and artists and other Spotify information',
 }
