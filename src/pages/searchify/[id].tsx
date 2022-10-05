8673f6931ccddc6b7b96cd3327d67b83679f998d
wesngu28 <serpents123@gmail.com>
2022-10-04 19:34:49 -0700

    forgot part of the link

--- src/pages/searchify/[id].tsx
+++ src/pages/searchify/[id].tsx
@@ -96,8 +96,7 @@ export default function Music ({music, type, adjectives}: Props) {
 export const getServerSideProps: GetServerSideProps = async ( { query }) => {
   let searchQuery = (query.id! as string).replace('/searchify/', '');
   const searchQueryArr = searchQuery.split('=');
-  console.log(`https://searchifyy.vercel.app/search/${searchQueryArr[0]}/${searchQueryArr[1]}`);
-  const fetchData = await fetch(`https://searchifyy.vercel.app/search/${searchQueryArr[0]}/${searchQueryArr[1]}?${query.links}`)
+  const fetchData = await fetch(`https://searchifyy.vercel.app/search/${searchQueryArr[0]}/${searchQueryArr[1]}?links=${query.links}`)
   let json;
   try {
     json = await fetchData.json()
